2040b5d62650ba2c57c9c3513aa022af25571874
goldfish9901 <745727212@qq.com>
2024-12-29 00:18:25 +0800

    init2

---
+++ app/src/main/java/org/goldfish/minesweeper_android_01/fragment/AbstractEntranceFragment.java
@@ -0,0 +1,26 @@
+package org.goldfish.minesweeper_android_01.fragment;
+
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.fragment.app.Fragment;
+
+import org.goldfish.minesweeper_android_01.MainApplication;
+
+abstract class AbstractEntranceFragment extends Fragment {
+    @NonNull
+    protected static AlertDialog.Builder getRestoreAlertBuilder(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(view.getContext());
+        builder.setTitle("警告");
+        builder.setMessage("重置所有储存数据?");
+        builder.setPositiveButton("Yes", (dialog, which) -> {
+            MainApplication.getInstance().getDao().deleteAll();
+            Toast.makeText(view.getContext(), "数据已重置", Toast.LENGTH_SHORT).show();
+        });
+        builder.setNegativeButton("No", (dialog, which) -> {
+        });
+        return builder;
+    }
+}
